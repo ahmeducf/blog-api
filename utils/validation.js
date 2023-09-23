4319154a067d9c1126dca4f2ce251094b8aa88b6
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-23 22:48:47 +0300

    chore: Add createPostValidationChain and some fixes

--- utils/validation.js
+++ utils/validation.js
@@ -3,12 +3,15 @@ const { body, validationResult } = require('express-validator');
 const createUsernameValidationChain = () =>
   body('username')
     .exists()
-    .trim()
-    .notEmpty()
     .withMessage('username is required')
     .bail()
     .isString()
     .withMessage('username must be a string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('username is required')
+    .bail()
     .escape()
     .toLowerCase();
 
@@ -19,11 +22,10 @@ const createPasswordValidationChain = () =>
     .bail()
     .isString()
     .withMessage('password must be a string')
+    .bail()
     .escape();
 
-const createDateValidationChain = (check, date) =>
-  check(date)
-    .isISO8601()
+const createDateValidationChain = (check, date) => check(date).isISO8601();
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
@@ -37,14 +39,52 @@ const validate = (req, res, next) => {
         fields: errorMessages,
       },
     });
+  } else {
+    next();
   }
-
-  next();
 };
+
+const createPostValidationChain = () => [
+  body('title')
+    .exists()
+    .withMessage('Title is required')
+    .bail()
+    .isString()
+    .withMessage('Title must be a string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('Title is required')
+    .bail()
+    .isLength({ min: 3, max: 100 })
+    .withMessage('Title must be between 3 and 100 characters')
+    .bail()
+    .escape(),
+  body('content')
+    .exists()
+    .withMessage('Content is required')
+    .bail()
+    .isString()
+    .withMessage('Content must be a string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('Content is required')
+    .bail()
+    .isLength({ min: 3, max: 1000 })
+    .withMessage('Content must be between 3 and 1000 characters')
+    .bail()
+    .escape(),
+  body('isPublished')
+    .optional()
+    .isBoolean()
+    .withMessage('isPublished must be a boolean'),
+];
 
 module.exports = {
   createUsernameValidationChain,
   createPasswordValidationChain,
   createDateValidationChain,
+  createPostValidationChain,
   validate,
 };
