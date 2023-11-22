126e551ad9ecdabd5cda94ee78a013ae469509c3
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-11-22 14:03:18 +0200

    fix: Create log directory if dosen't exist

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,13 @@
 const logger = require('morgan');
 const path = require('path');
-const { createWriteStream } = require('fs');
+const fs = require('fs');
 
 module.exports = () => {
-  const accessLogStream = createWriteStream(
+  if (!fs.existsSync(path.join(process.cwd(), 'logs'))) {
+    fs.mkdirSync(path.join(process.cwd(), 'logs'));
+  }
+
+  const accessLogStream = fs.createWriteStream(
     path.join(process.cwd(), 'logs', 'access.log'),
     { flags: 'a' },
   );
